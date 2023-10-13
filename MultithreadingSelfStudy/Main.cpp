0ddf2e96616138ea4962ebf51a30ec46099e9c8a
DylanoSol <dylanosolter@hotmail.com>
2023-10-13 14:14:22 +0200

    [CHANGED] Lock based system with condition variables
    Video series video 7 finished

--- MultithreadingSelfStudy/Main.cpp
+++ MultithreadingSelfStudy/Main.cpp
@@ -9,9 +9,6 @@
 #include <span>
 #include "Timer.h"
 
-//Following along with video tutorial series by ChiliTomatoNoodle
-constexpr size_t DATASET_SIZE = 50000000; 
-
 void ProcessDataset(std::span<int> set, int& sum)
 {
 	for (int x : set)
@@ -24,9 +21,110 @@ void ProcessDataset(std::span<int> set, int& sum)
 		sum += int(std::sin(std::cos(y)) * limit);
 
 		//Same thread unlocks the mutex. 
-	
+
 	}
 }
+
+//Following along with video tutorial series by ChiliTomatoNoodle
+class ControlObject
+{
+public:
+	ControlObject(int workerCount) : m_lk{ m_mtx }, m_workerCount{ workerCount }
+	{
+
+	}
+
+	void SignalDone()
+	{
+		{
+			std::lock_guard lk {m_mtx}; 
+			m_doneCount++; 
+		}
+
+		if (m_doneCount == m_workerCount)
+		{
+			//Notify the condition variable of this thread. 
+			m_cv.notify_one(); 
+		}
+	}
+
+	void WaitForAllDone()
+	{
+		//Wait until work is done. 
+		m_cv.wait(m_lk, [this] {return m_doneCount == m_workerCount; }); 
+		m_doneCount = 0; 
+	}
+
+private:
+	std::condition_variable m_cv; 
+	std::mutex m_mtx; 
+	std::unique_lock<std::mutex> m_lk; 
+	int m_workerCount; 
+	//SharedMemory 
+	int m_doneCount = 0; 
+
+};
+
+class Worker
+{
+public:
+	Worker(ControlObject* control) : m_PControl{ control }, m_thread{&Worker::Run, this} 
+	{
+
+	}
+
+	//Function will be called by the main thread. 
+	void SetJob(std::span<int> data, int* pOut)
+	{
+		{
+			std::lock_guard lk {m_mtx}; 
+			m_input = data; 
+			m_pOutput = pOut; 
+		}
+
+		m_cv.notify_one(); 
+	}
+
+	void Kill()
+	{
+		{
+			std::lock_guard lk {m_mtx};
+			m_threadDying = true; 
+		}
+
+		m_cv.notify_one();
+	}
+
+private:
+	void Run()
+	{
+		std::unique_lock lk {m_mtx}; 
+		while (true)
+		{
+			//1. Lock. 2. Extra condition. 
+			m_cv.wait(lk, [this] {return m_pOutput != nullptr || m_threadDying; }); 
+			if (m_threadDying) break; 
+			ProcessDataset(m_input, *m_pOutput); //Mutex remains locked when processing this. 
+			m_pOutput = nullptr; 
+			m_input = {}; //Zero out input. 
+			m_PControl->SignalDone(); 
+		}
+	}
+
+	ControlObject* m_PControl; 
+	std::jthread m_thread; 
+	std::condition_variable m_cv; 
+	std::mutex m_mtx; 
+
+	//Shared memory. 
+	std::span<int> m_input; 
+	int* m_pOutput = nullptr; 
+	bool m_threadDying = false; 
+};
+
+constexpr size_t DATASET_SIZE = 50000000; 
+
+
 
 
 std::vector<std::array<int, DATASET_SIZE>> GenerateDatasets()
@@ -78,10 +176,10 @@ int BigOperation()
 	return 0; 
 }
 
+
+
 int SmallOperation()
 {
-	
-
 
 	struct Value
 	{
@@ -94,24 +192,45 @@ int SmallOperation()
 	Timer timer;
 	timer.StartTimer();
 
+
+	constexpr size_t workerCount = 4;
+	ControlObject mControl{ workerCount };
+	//Unique_ptr is necessary here to prevent dangling pointers.
+	std::vector<std::unique_ptr<Worker>> workerPtrs;
+
+	for (size_t i = 0; i < workerCount; i++)
+	{
+		workerPtrs.push_back(std::make_unique<Worker>(&mControl)); //Pointers now going to be idling, waiting for jobs. 
+	}
+
+
 	auto datasets = GenerateDatasets(); 
 
 	int grandTotal = 0; 
 	std::vector<std::jthread> workers;
-	constexpr const auto subsetSize = DATASET_SIZE;
+	const auto subsetSize = DATASET_SIZE / 10000;
 	for (size_t i = 0; i < DATASET_SIZE; i += subsetSize)
 	{
 		for (size_t j = 0; j < 4; j++)
 		{
-			workers.push_back(std::jthread{ProcessDataset, std::span{&datasets[j][i], subsetSize}, std::ref(sum[j].v)});
+			workerPtrs[j]->SetJob(std::span{&datasets[j][i], subsetSize}, &sum[j].v);
 		}
-		workers.clear(); //Destroy the threads. 
-		grandTotal = sum[0].v + sum[1].v + sum[2].v + sum[3].v; 
+		mControl.WaitForAllDone(); //This guy will wake up when all jobs are done. 
 	}
+
+	grandTotal = sum[0].v + sum[1].v + sum[2].v + sum[3].v;
 
 	float timeElapsed = timer.GetTime();
 	printf("%f milliseconds \n", timeElapsed);
 	printf("%d sum \n", grandTotal);
+
+	//Don't leave the threads dangling. 
+
+	for (auto& w : workerPtrs)
+	{
+		w->Kill(); 
+	}
+	workerPtrs.clear(); 
 
 	return 0; 
 }
